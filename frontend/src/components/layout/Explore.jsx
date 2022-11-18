2e08a9e2a3a4ab3978ea9e813b214fb4850f9bb7
Rashaun Bennett <rashaunbennett1017@gmail.com>
2022-11-17 21:05:23 -0500

    Commit

--- frontend/src/components/layout/Explore.jsx
+++ frontend/src/components/layout/Explore.jsx
@@ -267,55 +267,6 @@ function Explore({ loading, exploreNFT, handleTheme, theme }) {
                 </div>
               </div>
             </motion.div>
-            {/* Single NFT */}
-            <motion.div variants={item} className="mt-12">
-              <div className=" z-40 flex flex-col justify-center items-center w-72 self-center p- bg-primary/10 rounded-3xl shadow-2xl shadow-violet-300/25">
-                <div className="mt-3 p-4 flex gap-2 flex-col justify-end h-64 w-64 overflow-hidden relative rounded-lg">
-                  <img
-                    src={Ape}
-                    alt="NFT"
-                    className="pointer-events-none  scale-125  opacity-100 translate-y-3 z-10"
-                  />
-                  {/* <video
-                        src={Fluff}
-                        autoPlay
-                        muted
-                        loop
-                        alt="NFT"
-                        className="pointer-events-none  scale-125 absolute translate-y-3"
-                      /> */}
-                </div>
-
-                <div className="self-start p-4 flex flex-col gap-2 w-full">
-                  <div className="flex flex-col gap-4">
-                    <p className="z-40 bottom-0s tracking-wider font-medium">
-                      Hape Beast
-                    </p>
-                    <div className="flex gap-2 items-center">
-                      <img
-                        className="rounded-full w-8 z-40"
-                        src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80"
-                        alt=""
-                      />
-                      <p className="z-40 text-sm font-thin tracking-wider">
-                        Jane Cooper
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-2 justify-between">
-                    <div className="flex items-center">
-                      <Ethereum className="fill-primary w-6" />
-                      <p className="">
-                        18.79 <span>ETH</span>
-                      </p>
-                    </div>
-                    <div>
-                      <button className="btn btn-xs">Place a Bid</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </motion.div>
           </motion.div>
         </div>
       </div>
